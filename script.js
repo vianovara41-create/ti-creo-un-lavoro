a7882bee1cb351e4cbfd1ef91dc2d01dd5d8f563
vianovara41-create <vianovara41@gmail.com>
2026-08-28 16:29:17 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-const PAYMENT_LINKS={box01:"",custom:""};function wire(id,key,label){const el=document.getElementById(id);if(PAYMENT_LINKS[key]){el.href=PAYMENT_LINKS[key];el.classList.remove("disabled");el.textContent=label}}wire("buy-box01","box01","Acquista la Box");wire("buy-custom","custom","Crea la mia Box");
+const PAYMENT_LINKS = {
+  box01: "https://buy.stripe.com/test_4gM8wI1fk3cQ43z37G4gg00",
+  custom: ""
+};
+
+function wire(id, key, label) {
+  const el = document.getElementById(id);
+  if (el && PAYMENT_LINKS[key]) {
+    el.href = PAYMENT_LINKS[key];
+    el.classList.remove("disabled");
+    el.textContent = label;
+  }
+}
+
+wire("buy-box01", "box01", "Acquista la Box");
+wire("buy-custom", "custom", "Crea la mia Box");
